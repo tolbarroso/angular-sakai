00f9865f5fefeea61543839f03a7fc0b72daa37b
carol barroso <carol.00000829955@unicap.br>
2024-06-21 22:34:42 +0000

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'undefined',
-    authDomain: 'undefined',
-    databaseURL: 'undefined',
-    projectId: 'undefined',
-    storageBucket: 'undefined',
-    messagingSenderId: 'undefined',
-    appId: 'undefined'
+    apiKey: "AIzaSyD5KHEgdgatAooqZuFSrAQEydWyDnQBTvw",
+    authDomain: "pizzaria-crud-61de9.firebaseapp.com",
+    databaseURL: "https://pizzaria-crud-61de9.firebaseio.com",
+    projectId: "pizzaria-crud-61de9",
+    storageBucket: "pizzaria-crud-61de9.appspot.com",
+    messagingSenderId: "105389223970",
+    appId: "1:105389223970:web:91f1e77c038ed7918fda7b",
   }
 };
